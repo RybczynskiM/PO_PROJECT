e4085e1eb22924576204699c4efa6ce4f4010933
Michał Rybczyński <m.rybczynski@aol.com>
2014-05-26 13:34:27 +0200

    Projekt Matka
    W Visual Studio otworzyc projekt, który znajduje się w folderze Github w
    folderze Documents/PO_PROJECT/PROJECT_PO_EKA_INWENTORY/

---
+++ PROJECT_PO_EKA_INWENTORY/PROJECT_PO_EKA_INWENTORY/MyForm.cpp
@@ -0,0 +1,14 @@
+#include "MyForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThread]
+void Main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	PROJECT_PO_EKA_INWENTORY::MyForm form;
+	Application::Run(%form);
+}
