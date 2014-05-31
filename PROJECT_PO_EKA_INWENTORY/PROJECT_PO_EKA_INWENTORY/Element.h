038f1cb26c3a6ff706426c8ed5a75b0989edd3b1
Mswietek <mateo.swietek@gmail.com>
2014-05-31 16:41:22 +0200

    Utw Elementy

--- PROJECT_PO_EKA_INWENTORY/PROJECT_PO_EKA_INWENTORY/Element.h
+++ PROJECT_PO_EKA_INWENTORY/PROJECT_PO_EKA_INWENTORY/Element.h
@@ -1,12 +1,54 @@
+class  Element :
+{     public:
+	int rodzaj;
+	int ilosc;
 
-
-class Element
-{
-private:
-	int typ;
-public:
-	Element();
-	
-	int get_typ();
 };
 
+class Rezystor : public Element
+{
+public:
+	float rezystancja;
+	float tolerancja_r;
+	float moc_znamion;
+};
+
+class Kondensator : public Element
+{
+public:
+	float pojemnosc;
+	float nap_znamion;
+	float stratnosc;
+	float tolerancja;
+};
+
+class Cewka : public Element
+{
+public:
+	float indukcyjnosc;
+	float reaktancja;
+	float impedancja;
+	float dobroc;
+};
+
+class Tranzystor : public Element
+{
+public:
+	float prad_kolekt;
+	float napiecie_uce;
+	float prad_bazy;
+	float napiecie_nasycenia;
+	float dop_straty_mocy;
+};
+
+class Dioda : public Element
+{   public:
+	float prad_przebicia;
+	float opornosc;
+};
+
+class Tyrystor : public Element
+{   public:
+	float prad_gran_obciazenia;
+	float graniczne_nap_pow;
+};
